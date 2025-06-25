543e7633fe239816bdcc3716b72fa0ec37bbd780
arifaji29 <arifajif@gmail.com>
2025-06-26 02:33:50 +0700

    tambah lagi

--- components/WebDevProjects.tsx
+++ components/WebDevProjects.tsx
@@ -14,6 +14,13 @@ const webDevProjectsData = [
     githubUrl: 'https://github.com/arifaji29/My-List',
     liveUrl: 'https://belanjaemak.vercel.app/',
   },
+   {
+    image: '/images/web1.png',
+    title: 'Aplikasi Web - Daftar Belanja Emak',
+    description: '"Daftar Belanja Emak" adalah aplikasi web front-end interaktif yang berfungsi sebagai daftar belanjaan digital. Didesain dengan antarmuka yang modern dan mudah digunakan, aplikasi ini membantu pengguna untuk mencatat, melacak, dan mengelola daftar barang yang perlu dibeli secara efisien. Proyek ini dibangun sepenuhnya menggunakan teknologi web fundamental: HTML, CSS, dan JavaScript.',
+    githubUrl: 'https://github.com/arifaji29/My-List',
+    liveUrl: 'https://belanjaemak.vercel.app/',
+  },
   // Tambahkan proyek web dev lainnya
 ];
 
