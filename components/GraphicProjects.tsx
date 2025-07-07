8d39c38a0233884dfe4947e5c3035622f1781ce5
arifaji29 <arifajif@gmail.com>
2025-07-07 21:50:18 +0700

    ganti judul

--- components/GraphicProjects.tsx
+++ components/GraphicProjects.tsx
@@ -7,32 +7,32 @@ const graphicProjectsData = [
 
    {
     image: '/images/design2.png',
-    title: 'Giggle Outen',
+    title: 'Giggle Outen Logo Design',
     fullDescription: 'Salah satu project yang saya kerjakan adalah pembuatan logo untuk produk edukatif anak bernama GIGGLE OUTEN. Produk ini berupa busy board mainan edukatif yang dirancang untuk melatih sensorik dan motorik anak-anak. Saya merancang logo yang mencerminkan kesan ceria, aman, dan ramah anak, sesuai dengan karakter produk dan target audiensnya.',
   },
    {
     image: '/images/design3.png',
-    title: 'Logo Design',
+    title: 'Gruvio Logo Design',
     fullDescription: 'Salah satu project yang saya kerjakan adalah pembuatan logo untuk produk edukatif anak bernama GIGGLE OUTEN. Produk ini berupa busy board mainan edukatif yang dirancang untuk melatih sensorik dan motorik anak-anak. Saya merancang logo yang mencerminkan kesan ceria, aman, dan ramah anak, sesuai dengan karakter produk dan target audiensnya.',
   },
     {
     image: '/images/design1.png',
-    title: 'Social Media Design',
+    title: 'Jiecos Social Media Design',
     fullDescription: 'Salah satu project yang sering saya kerjakan adalah membuat feed atau carousel di Instagram. Salah satu project yang saya kerjakan dalah feed Instagram JIECOS BEAUTY. Produk skincare pendatang baru yang memulai brandingnya melalui sosial media ',
   },
    {
     image: '/images/design4.png',
-    title: 'Logo Design',
+    title: 'Kaosenzo katalogue',
     fullDescription: 'Salah satu project yang saya kerjakan adalah pembuatan logo untuk produk edukatif anak bernama GIGGLE OUTEN. Produk ini berupa busy board mainan edukatif yang dirancang untuk melatih sensorik dan motorik anak-anak. Saya merancang logo yang mencerminkan kesan ceria, aman, dan ramah anak, sesuai dengan karakter produk dan target audiensnya.',
   },
   {
     image: '/images/design5.png',
-    title: 'Logo Design',
+    title: 'Marketplace Banner',
     fullDescription: 'Salah satu project yang saya kerjakan adalah pembuatan logo untuk produk edukatif anak bernama GIGGLE OUTEN. Produk ini berupa busy board mainan edukatif yang dirancang untuk melatih sensorik dan motorik anak-anak. Saya merancang logo yang mencerminkan kesan ceria, aman, dan ramah anak, sesuai dengan karakter produk dan target audiensnya.',
   },
    {
     image: '/images/design6.png',
-    title: 'Logo Design',
+    title: 'Instagram Stories Design',
     fullDescription: 'Salah satu project yang saya kerjakan adalah pembuatan logo untuk produk edukatif anak bernama GIGGLE OUTEN. Produk ini berupa busy board mainan edukatif yang dirancang untuk melatih sensorik dan motorik anak-anak. Saya merancang logo yang mencerminkan kesan ceria, aman, dan ramah anak, sesuai dengan karakter produk dan target audiensnya.',
   }
   // Tambahkan proyek desain grafis lainnya di sini
