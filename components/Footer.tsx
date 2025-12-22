9697474b00f9540b1277b9bf8bbe78581281a9c4
arifaji29 <arifajif@gmail.com>
2025-12-22 08:02:39 +0700

    update styling saja

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -13,13 +13,13 @@ const Footer = () => {
       {/* Kontainer baru untuk ikon media sosial */}
       <div className={styles.socialLinks}>
         <Link href="https://www.instagram.com/arifaji29/" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
-          <FaInstagram size={24} />
+          <FaInstagram size={20} />
         </Link>
         <Link href="https://www.behance.net/arifajifadilah29" target="_blank" rel="noopener noreferrer" aria-label="Behance">
-          <FaBehance size={24} />
+          <FaBehance size={20} />
         </Link>
         <Link href="https://github.com/arifaji29" target="_blank" rel="noopener noreferrer" aria-label="GitHub">
-          <FaGithub size={24} />
+          <FaGithub size={20} />
         </Link>
       </div>
 
